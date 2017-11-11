bbed867d48894b778036d09841747ae3a0ba1716
Johannes Braun <j.braun@agentur-halma.de>
2017-11-11 21:17:42 +0100

    changed email address for contact form to production value

--- phpincludes/contactform/contactform_controller.php
+++ phpincludes/contactform/contactform_controller.php
@@ -13,7 +13,7 @@ class ContactformController extends Controller {
 	 * @var string
 	 * Contact mail recipient address
 	 */
-	protected $recipientEmail = 'johannes.braun@hannenz.de';	
+	protected $recipientEmail = 'peter.schrodi@t-online.de';	
 	
 	/*
 	 * @var Object
